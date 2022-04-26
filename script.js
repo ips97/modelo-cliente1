e9719ebaf91938c67bb084978f1b195f3e4843ab
Italo PInheiro <italo_00pinheiro@hotmail.com>
2022-04-26 00:50:23 -0300

    make it better

---
+++ script.js
@@ -0,0 +1,29 @@
+function mostrar(){
+    var paragrafo = document.getElementById("texto");
+
+    if(paragrafo.style.display == "none"){
+        paragrafo.style.display = "inline-block"
+    }else{
+    paragrafo.style.display = "none"
+    }
+}
+
+function mostrar2(){
+    var paragrafo = document.getElementById("texto2");
+
+    if(paragrafo.style.display == "none"){
+        paragrafo.style.display = "inline-block"
+    }else{
+    paragrafo.style.display = "none"
+    }
+}
+
+function mostrar3(){
+    var paragrafo = document.getElementById("texto3");
+
+    if(paragrafo.style.display == "none"){
+        paragrafo.style.display = "inline-block"
+    }else{
+    paragrafo.style.display = "none"
+    }
+}
